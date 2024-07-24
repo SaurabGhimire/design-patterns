f624ce0499275b1b80a9c6088d8a9c3a973c7abd
Saurab Ghimire <sourrab@gmail.com>
2024-07-24 13:08:45 -0500

    reorganize folder structures

--- car-rental/src/main/java/com/sourrab/car_rental/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sourrab.car_rental.repository;
-
-import com.sourrab.car_rental.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
